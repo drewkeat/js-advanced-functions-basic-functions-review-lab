1d3d81400ecf4444e5551edecd1a6dd5a551f513
Drew Keat <thedrewkeat@gmail.com>
2021-09-23 10:48:20 -0600

    commit tree 359635db484816d63bcc1a2953dcfdde0df34674

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(string = "*") {
+  return function (adjective = "special") {
+    return `You are ${string}${adjective}${string}!`;
+  };
+}
+
+const Calculator = {
+  add: (...args) => {
+    return args.reduce(function sum(accum, number) {
+      return accum + number;
+    });
+  },
+  subtract: (...args) => {
+    return args.reduce(function subtract(accum, number) {
+      return accum - number;
+    });
+  },
+  multiply: (...args)=> {
+      return args.reduce(function multiply(accum, number) {
+          return accum * number;
+      })
+  },
+  divide: (...args)=> {
+    return args.reduce(function multiply(accum, number) {
+        return accum / number;
+    })
+}
+};
+
+function actionApplyer(start = 0, array) {
+    if (array.length === 0) {
+        return start
+    }
+}
